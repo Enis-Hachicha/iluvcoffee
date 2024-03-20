f36f29c6d41839d9984eb71e44fa1e68c5e04373
Enis Hachicha <hachicha.enis@gmail.com>
2024-03-20 21:49:09 +0100

    dto update
    implemented data validator into the create-coffee.dto

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,11 @@
+import { IsString } from 'class-validator';
 export class CreateCoffeeDto {
+  @IsString() //the @IsString decorator will validate if our data is a string or not
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
+
+  @IsString({ each: true })
   readonly flavors: string[];
 }
